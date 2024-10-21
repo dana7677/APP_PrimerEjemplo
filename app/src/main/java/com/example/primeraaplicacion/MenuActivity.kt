542f3b62a694ed19a159470fd5ef6b4973fab366
Daniel <danote76772@gmail.com>
2024-10-21 20:53:24 +0200

    DosBotones Hombre y Mujer diseño Xml

--- app/src/main/java/com/example/primeraaplicacion/MenuActivity.kt
+++ app/src/main/java/com/example/primeraaplicacion/MenuActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.primeraaplicacion.ImcCalculator.ImcCalculatorActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +16,13 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         val SaludarApp = findViewById<Button>(R.id.BTMSaludar)
+        val CalcularIMCApp = findViewById<Button>(R.id.BTMCalcularIMC)
 
         SaludarApp.setOnClickListener {
             NavigateToSaludarApp()
+        }
+        CalcularIMCApp.setOnClickListener {
+            NavigateToCalcularImcApp()
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -26,7 +31,13 @@ class MenuActivity : AppCompatActivity() {
             insets
         }
     }
-    fun NavigateToSaludarApp()
+
+    private fun NavigateToCalcularImcApp() {
+        val intent = Intent(this,ImcCalculatorActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun NavigateToSaludarApp()
     {
         val intent = Intent(this,FirstAppActivity::class.java)
         startActivity(intent)
