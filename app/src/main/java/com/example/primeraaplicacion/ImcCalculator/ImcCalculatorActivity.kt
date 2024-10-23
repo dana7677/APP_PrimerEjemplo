d1a25a50a91a64f02bf700615a61180bab3b0452
Daniel <danote76772@gmail.com>
2024-10-23 20:52:56 +0200

    Botones de Edad y Peso creados, tambien Slider de Altura

--- app/src/main/java/com/example/primeraaplicacion/ImcCalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/example/primeraaplicacion/ImcCalculator/ImcCalculatorActivity.kt
@@ -1,6 +1,8 @@
 package com.example.primeraaplicacion.ImcCalculator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
@@ -8,14 +10,34 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.primeraaplicacion.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.RangeSlider
+import com.google.android.material.slider.Slider
+import java.text.DecimalFormat
 
 class ImcCalculatorActivity : AppCompatActivity() {
 
     private var isMaleSelected:Boolean=true
     private var isFemaleSelected:Boolean=false
+    private var currentWeight:Int =60
+    private var currentAge:Int =30
+    private var currentHeight:Int=120
 
     private lateinit var viewMale:CardView
     private lateinit var viewFemale:CardView
+    private lateinit var TxtHeight:TextView
+    private lateinit var rsSlider:RangeSlider
+
+    private lateinit var txtAge:TextView
+    private lateinit var txtWeight:TextView
+    private lateinit var btnSubstractAge: FloatingActionButton
+    private lateinit var btnUpGradeAge: FloatingActionButton
+    private lateinit var btnSubstractWeight: FloatingActionButton
+    private lateinit var btnUpGradeWeight: FloatingActionButton
+    private lateinit var btnCalculateImc:Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,8 +45,6 @@ class ImcCalculatorActivity : AppCompatActivity() {
         initComponents()
         setInitListeners()
         initUI()
-
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -34,14 +54,69 @@ class ImcCalculatorActivity : AppCompatActivity() {
     }
     private fun initComponents()
     {
+        //Boton selección de genero
         viewMale=findViewById(R.id.ViewOfMale)
         viewFemale=findViewById(R.id.ViewOfFemale)
+
+        //AlturaTxt && SliderAltura
+        TxtHeight=findViewById(R.id.txtHeight)
+        rsSlider=findViewById(R.id.rSHeight)
+
+        //Botones && Txt de Suma/Resta Edad y Peso
+        txtAge=findViewById(R.id.txtAge)
+        txtWeight=findViewById(R.id.txtWeight)
+        btnSubstractAge=findViewById(R.id.btnSubAge)
+        btnUpGradeAge=findViewById(R.id.btnPlusAge)
+        btnSubstractWeight=findViewById(R.id.btnSubWeight)
+        btnUpGradeWeight=findViewById(R.id.btnPlusWeight)
+        btnCalculateImc=findViewById(R.id.btnCalcuImc)
+
     }
+
     private fun setInitListeners()
     {
-        viewMale.setOnClickListener {setGenderColor(isMaleSelected)  }
-        viewFemale.setOnClickListener {setGenderColor(isFemaleSelected)  }
+        viewMale.setOnClickListener {changeGenderColor()
+            setGenderColor(isMaleSelected) }
+        viewFemale.setOnClickListener {changeGenderColor()
+            setGenderColor(isFemaleSelected) }
+        rsSlider.addOnChangeListener{_,value,_->
+            val dc=DecimalFormat("#.##")
+            val result=dc.format(value)
+            currentHeight=result.toInt()
+            TxtHeight.text= "$currentHeight cm"
+        }
+        btnSubstractWeight.setOnClickListener { currentWeight--
+            SetWeight()}
+        btnSubstractAge.setOnClickListener { currentAge--
+            SetAge()}
+        btnUpGradeWeight.setOnClickListener { currentWeight++
+            SetWeight()}
+        btnUpGradeAge.setOnClickListener { currentAge++
+            SetAge()}
+        btnCalculateImc.setOnClickListener { CalculateImc() }
+
     }
+    private fun CalculateImc()
+    {
+
+    }
+    private fun SetWeight()
+    {
+        txtWeight.text="$currentWeight KG"
+    }
+    private fun SetAge()
+    {
+        txtAge.text="$currentAge KG"
+    }
+
+
+    private fun changeGenderColor()
+    {
+        isMaleSelected = !isMaleSelected
+        isFemaleSelected = !isFemaleSelected
+
+    }
+
     private fun setGenderColor(isViewSelected:Boolean) {
         viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
         viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected))
